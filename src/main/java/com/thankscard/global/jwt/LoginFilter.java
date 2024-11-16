627c7ddaad2da47e93aced23e43c3b9cedc6009a
MeongW <125392666+MeongW@users.noreply.github.com>
2024-11-16 13:48:07 +0900

    🐛Fix: refreshToken 만료 오류 수정

--- src/main/java/com/thankscard/global/jwt/LoginFilter.java
+++ src/main/java/com/thankscard/global/jwt/LoginFilter.java
@@ -79,20 +79,10 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
         GrantedAuthority auth = iterator.next();
 
         String role = auth.getAuthority();
+        String refreshToken = jwtUtil.createJwt(username, role, 1000*60*60*12L);
+        String accessToken = jwtUtil.createJwt(username, role, 1000*60*60*3L);
 
-        String refreshToken = customUserDetails.getRefreshToken();
-        String accessToken = null;
-
-        if (refreshToken != null && !refreshToken.isEmpty() && !jwtUtil.isExpired(refreshToken)) {
-
-            accessToken = jwtUtil.createJwt(username, role, 1000*60*60*3L);
-        } else {
-
-            refreshToken = jwtUtil.createJwt(username, role, 1000*60*60*12L);
-            accessToken = jwtUtil.createJwt(username, role, 1000*60*60*3L);
-
-            customUserDetailService.setRefreshToken(customUserDetails.getUser(), refreshToken);
-        }
+        customUserDetailService.setRefreshToken(customUserDetails.getUser(), refreshToken);
 
         response.addHeader("Authorization", "Bearer " + accessToken);
 
